21cc8d2dc085c4cfc026de7f0ec748765879e3b6
syatinosuke <ngn2149560@stu.o-hara.ac.jp>
2022-07-29 11:32:36 +0900

    a

--- private_grayscale/settings_common.py
+++ private_grayscale/settings_common.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'private_grayscale.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'private_grayscale',
+        'NAME': 'grayscale',
         'USER':os.environ.get('DB_USER'),
         'PASSWORD':os.environ.get('DB_PASSWORD'),
         'HOST':'',
